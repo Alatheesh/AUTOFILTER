c4e2f72a97a88c5209d02b7c1370516841431835
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2026-06-20 16:37:37 +0530

    Integrate asyncio for bot execution in start.py
    Refactored start.py to use asyncio for bot execution.

--- start.py
+++ start.py
@@ -1,18 +1,13 @@
 from threading import Thread
 from web import app
 import os
-
-print("START.PY STARTED")
+import asyncio
+import bot
 
 def run_web():
     port = int(os.environ.get("PORT", 7860))
-    print("STARTING FLASK")
     app.run(host="0.0.0.0", port=port)
 
 Thread(target=run_web, daemon=True).start()
 
-print("IMPORTING BOT")
-
-import bot
-
-print("BOT IMPORTED")
+asyncio.run(bot.main())
