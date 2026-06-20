b6ad460f4eea826504b4f4e0b497b5c914da3cf9
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2026-06-20 17:03:04 +0530

    Simplify server startup by removing threading

--- start.py
+++ start.py
@@ -1,13 +1,5 @@
-from threading import Thread
 from server import app
 import os
-import asyncio
-import bot
 
-def run_web():
-    port = int(os.environ.get("PORT", 7860))
-    app.run(host="0.0.0.0", port=port)
-
-Thread(target=run_web, daemon=True).start()
-
-asyncio.run(bot.main())
+port = int(os.environ.get("PORT", 7860))
+app.run(host="0.0.0.0", port=port)
